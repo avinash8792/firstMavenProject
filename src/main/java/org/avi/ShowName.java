42a75635292b582028e4116b1037548ef82d1723
avinash8792 <avinash8792@gmail.com>
2017-03-02 02:03:55 +0000

    Signed-off-by: avinash8792 <avinash8792@gmail.com>

---
+++ src/main/java/org/avi/ShowName.java
@@ -0,0 +1,9 @@
+package org.avi;
+
+public class ShowName {
+
+	public String name(){
+		return "Avinash";
+	}
+	
+}
---
+++ src/test/java/org/avi/Test.java
@@ -0,0 +1,12 @@
+package org.avi;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
